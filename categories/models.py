92817f5adce906bc4d2c20451d99728c783d0adc
jumyeong <jm01028250379@gmail.com>
2021-12-15 15:26:57 +0900

    [Modify] modeling

--- categories/models.py
+++ categories/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 class Category(models.Model): 
-    talant     = models.CharField(max_length=45)
+    talent     = models.CharField(max_length=45)
     
     class Meta: 
         db_table = 'categories'
