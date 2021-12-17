7ed09102d23e2cf7cf8fc785009e257f3aa6e144
jumyeong <jm01028250379@gmail.com>
2021-12-17 17:27:32 +0900

    [Create]  imagehandler

--- users/urls.py
+++ users/urls.py
@@ -1,7 +1,8 @@
 from django.urls    import path
 
-from users.views    import KakaoLoginView
+from users.views    import KakaoLoginView, ImageUploader
 
 urlpatterns = [
-    path('/signin', KakaoLoginView.as_view()) 
+    path('/signin', KakaoLoginView.as_view()),
+    path('/imageUploader', ImageUploader.as_view()),
 ]
