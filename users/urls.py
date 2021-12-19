0ed86d47d3793066b5dd299002f2cd7864bc6577
bakha1002 <yh795483@naver.com>
2021-12-19 15:11:34 +0900

    호스트 리스트 페이지 구현

--- users/urls.py
+++ users/urls.py
@@ -1,8 +1,9 @@
 from django.urls    import path
 
-from users.views    import KakaoLoginView, ImageUploader
+from users.views    import KakaoLoginView, ImageUploader, HostListView
 
 urlpatterns = [
     path('/signin', KakaoLoginView.as_view()),
     path('/imageUploader', ImageUploader.as_view()),
+    path('/hosts', HostListView.as_view()),
 ]
